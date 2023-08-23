5cd8c6611e0bc6a828ffc5a52c18346d1d0f8c26
m.r.amin96.ma <m.r.amin96.ma@gmail.com>
2023-08-23 14:34:47 +0300

    htaccess

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { parse } from 'url';
+import next from 'next';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
+  createServer((req: IncomingMessage, res: ServerResponse) => {
+    const parsedUrl = parse(req.url || '', true);
     const { pathname } = parsedUrl;
-      console.log(pathname);
-      // Pass the request to Next.js as usual
-      handle(req, res, parsedUrl);
-  }).listen(3000, (err) => {
+    console.log(pathname);
+    
+    // Pass the request to Next.js as usual
+    handle(req, res, parsedUrl);
+  }).listen(3000, (err: Error | null) => {
     if (err) throw err;
     console.log('> Ready on http://localhost:3000');
   });
