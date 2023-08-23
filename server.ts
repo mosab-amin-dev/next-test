2d1eb71e96d8130820cdfba377f6522ddbdc21d5
m.r.amin96.ma <m.r.amin96.ma@gmail.com>
2023-08-23 14:12:56 +0300

    htaccess

---
+++ server.ts
@@ -0,0 +1,20 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+      console.log(pathname);
+      // Pass the request to Next.js as usual
+      handle(req, res, parsedUrl);
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
